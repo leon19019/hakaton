76e7076515157f63cd5b96b4f741a02bb12578de
leon19019 <leon18018@yandex.ru>
2020-01-25 15:48:05 +0300

    not for varya

--- game.py
+++ game.py
@@ -34,14 +34,18 @@ screen = pygame.display.set_mode((1280, 1024))
 pygame.display.set_caption("Asteroids")
 
 sky = scale(pygame.image.load("/home/asp-159/hakaton/images/photo_2020-01-25_14-17-55.jpg"), (1280, 1024))
+end = scale(pygame.image.load("/home/asp-159/Загрузки/Telegram Desktop/photo_2020-01-25_15-38-15.jpg", (1280, 1024))
 # создаем корабль в точке 400 400
 ship = Spaceship(400, 400)
+
 
 # заведем переменные, чтобы помнить, какие клавиши нажаты
 left = False
 right = False
 down = False
 up = False
+def game_over:
+    screen.blit(end, (0, 0)) 
 
 while True:
     for e in pygame.event.get():
