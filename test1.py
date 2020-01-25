11bcefcdf32c5a27b1b885f3cd6f896e9648c4c0
leon19019 <leon18018@yandex.ru>
2020-01-25 12:42:54 +0300

    test

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ screen = pygame.display.set_mode((1280, 1024))
 clock = pygame.time.Clock()
 background_image = pygame.image.load('/home/asp-159/hakaton/images/photo_2020-01-25_11-53-51.jpg')
 class gamer:
-    pass
+    
 
 while True:
     screen.blit(background_image, (0, 0))
