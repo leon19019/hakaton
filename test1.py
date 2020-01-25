16222eb2b91eced91a4fec47388b10eb78b40fa1
leon19019 <leon18018@yandex.ru>
2020-01-25 12:24:39 +0300

    test

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 1024))
 clock = pygame.time.Clock()
 background_image = pygame.image.load('/home/asp-159/hakaton/images/photo_2020-01-25_11-53-51.jpg')
-class gamer():
+class gamer:
     pass
 
 while True:
