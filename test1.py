2097df5a4b43d6dede38411e33ba469673131876
leon19019 <leon18018@yandex.ru>
2020-01-25 13:39:51 +0300

    code for sanya

--- test1.py
+++ test1.py
@@ -24,24 +24,6 @@ while True:
     screen.blit(p1Paddle, (250, 500))
     pygame.display.update()
     clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                p1Paddle.x_velocity = -10
-            elif event.key == pygame.K_RIGHT:
-                p1Paddle.x_velocity = 10
-        if event.type == pygame.KEYUP:
-            if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
-                # if either the left or right arrow keys are released
-                p1Paddle.x_velocity = 0
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    #p1Paddle.x_distance += p1Paddle.x_velocity
-    # other stuff here
-    # drawing code here
-    pygame.display.update()
-
     pygame.display.update()
 while not done:
    for event in pygame == pygame.Quit:
