1a984f594f6713bc5e3d552b75c023ff71d312c0
leon19019 <leon18018@yandex.ru>
2020-01-25 18:42:21 +0300

    oleg

--- game.py
+++ game.py
@@ -1,49 +1,57 @@
 import pygame
 from pygame.transform import scale
 
+
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
 
         self.rect = pygame.Rect(x, y, 150, 150)
-        self.image = scale(pygame.image.load("/home/asp-159/hakaton/images/photo_2020-01-25_13-40-53.jpg"), (150, 150))
+        self.image = scale(pygame.image.load(
+            "/home/asp-159/hakaton/images/photo_2020-01-25_13-40-53.jpg"), (150, 150))
         self.xvel = 0
         self.yvel = 0
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
+
     def update(self, left, right, up, down):
         if left:
-            self.xvel = -12
+            self.xvel = -15
         if right:
-            self.xvel = 12
+            self.xvel = 15
         if up:
-            self.yvel = -12
+            self.yvel = -15
         if down:
-            self.yvel = 12
-        if shoot_left:
-            pass
-        if shoot_right:
-            pass
-        if shoot_up:
-            pass
-        if shoot_down:
-            pass
+            self.yvel = 15
+        # if shoot_left:
+            # pass
+        # if shoot_right:
+            # pass
+        # if shoot_up:
+            # pass
+        # if shoot_down:
+            # pass
         if not (left or right):
             self.xvel = 0
         if not (down or up):
             self.yvel = 0
         self.rect.x += self.xvel
         self.rect.y += self.yvel
+
     def game_over():
-        screen.blit(end, (0, 0)) 
+        screen.blit(end, (0, 0))
+
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 1024))
 pygame.display.set_caption("Asteroids")
 
-sky = scale(pygame.image.load("/home/asp-159/hakaton/images/photo_2020-01-25_14-17-55.jpg"), (1280, 1024))
-end = scale(pygame.image.load("/home/asp-159/Загрузки/Telegram Desktop/photo_2020-01-25_15-38-15.jpg"), (1280, 1024))
+sky = scale(pygame.image.load(
+    "/home/asp-159/hakaton/images/photo_2020-01-25_14-17-55.jpg"), (1280, 1024))
+end = scale(pygame.image.load(
+    "/home/asp-159/Загрузки/Telegram Desktop/photo_2020-01-25_15-38-15.jpg"), (1280, 1024))
+door_up = pygame.image.load("/home/asp-159/hakaton/images/Door(up, down).png")
 # создаем сыр в точке 400 400
 ship = Spaceship(640, 512)
 
@@ -81,14 +89,14 @@ while True:
         if e.type == pygame.KEYUP and e.key == pygame.K_s:
             down = False
         if e.type == pygame.KEYDOWN and e.key == pygame.K_4:
-
+            pass
         if lives == 0:
             game_over()
         if e.type == pygame.QUIT:
             raise SystemExit("QUIT")
     # рисуем фон
     screen.blit(sky, (0, 0))
-
+    screen.blit(door_up, (100, 0))
     # перемещаем сыр
     ship.update(left, right, up, down)
     # просим сыр нарисоваться
